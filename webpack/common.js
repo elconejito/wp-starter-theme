85a4c3b7ab9f514a0f8e555d89f7c86e71d704c7
Harvey Ramos <harvey@harvsworld.com>
2018-01-13 22:58:31 -0500

    Add ESLint

--- webpack/common.js
+++ webpack/common.js
@@ -7,28 +7,27 @@ const ManifestPlugin = require('webpack-manifest-plugin');
 module.exports = {
   entry: {
     main: './assets/index.js',
-    vendors: [ 'jquery', 'bootstrap', 'popper.js' ]
+    vendors: [ 'jquery', 'bootstrap', 'popper.js' ],
   },
   output: {
-    path: path.resolve(__dirname, '../dist')
+    path: path.resolve(__dirname, '../dist'),
   },
-  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
           use: [
-            'css-loader',
-            'postcss-loader'
+            { loader: 'css-loader', options: { sourceMap: true } },
+            { loader: 'postcss-loader', options: { sourceMap: true } },
           ],
-        })
+        }),
       },
       {
         test: /\.scss$/,
@@ -37,9 +36,9 @@ module.exports = {
           use: [
             { loader: 'css-loader', options: { sourceMap: true } },
             { loader: 'postcss-loader', options: { sourceMap: true } },
-            { loader: 'sass-loader', options: { sourceMap: true } }
+            { loader: 'sass-loader', options: { sourceMap: true } },
           ],
-        })
+        }),
       },
       {
         test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
@@ -49,24 +48,29 @@ module.exports = {
             options: {
               name: '[name].[ext]',
               outputPath: 'fonts/',    // where the fonts will go
-              publicPath: '../'        // override the default path
-            }
-          }
-        ]
-      }
-    ]
+              publicPath: '../',        // override the default path
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
-    new CleanWebpackPlugin(['dist'], { root: path.resolve(__dirname, '..'), }),
+    new CleanWebpackPlugin(
+      ['dist'],
+      {
+        root: path.resolve(__dirname, '..'),
+      },
+    ),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
-      Popper: 'popper.js'
+      Popper: 'popper.js',
     }),
     new ManifestPlugin({
       filter: ({ path }) => {
         return !path.startsWith('fonts');
-      }
-    })
-  ]
+      },
+    }),
+  ],
 };
